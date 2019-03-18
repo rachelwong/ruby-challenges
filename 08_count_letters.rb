7afb06f605e25146a505925a410a5e9f4c5294c9
rachelwong <hello.rachelwong@gmail.com>
2019-03-18 11:36:35 +1000

    attempted 08

--- 08_count_letters.rb
+++ 08_count_letters.rb
@@ -12,6 +12,28 @@
 
 def count_letters (string)
   result = {} # You'll need an empty hash to get started!
-  
+  array = string.chars() # split string into an array of characters
+  puts array
+
+  # in every character/item of the string array
+  for item in array do 
+    puts "I have found #{item}."
+
+    # if the character is in the result hash as a key
+    if result.has_key?(item)
+
+      # increment the key's vaue with 1
+      result[item] += 1
+      puts result
+
+    # if the character is NOT in the result hash as a key
+    else
+
+      # add to result hash as a new key with value of one 
+      result[item] = 1
+    end
   return result # return the hash
+  end
 end
+
+count_letters("hello")
